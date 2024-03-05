c55f6d1885205826ece08f43eb2de643d9a8584e
solregem <greenberg04@gmail.com>
2024-03-05 13:40:13 +0000

    1d diff eq plot

--- 1d-plot.py
+++ 1d-plot.py
@@ -8,26 +8,27 @@ def plot_multiple_lines(xdata, ydata):
     for i in range(len(ydata[0])):
         ax.plot(xdata, ydata[:,i])
     
-    ax.set_xlabel('n (iterations)')
-    ax.set_ylabel('Y')
+    ax.set_xlabel('time')
+    ax.set_ylabel('x')
     ax.set_title('Initial Condition Sensitivity Plot')
     
     plt.tight_layout()
     plt.show()
 
-def IteratedFunc( x ):
-    return x;
+def TimeDerivative( x ):
+    return np.cos( x );
 
-def Iterate( init_cond, N, X ):
+def Iterate( init_cond, N, dt, X ):
     f = np.zeros( (N,X) );
     f[ 0 ] = init_cond;
     for i in range( 1, N ):
-        f[ i ] = IteratedFunc( f[ i - 1 ] );
+        f[ i ] = f[ i - 1 ] + TimeDerivative( f[ i - 1 ] ) * dt;
     return f;
 
 N = 100;
+dt = 0.1;
 X = 100;
 x = np.linspace( 9.9, 10.1, X );
-y = Iterate( x, N=N, X=X );
+y = Iterate( x, N=N, dt=0.1, X=X );
 
-plot_multiple_lines( np.arange( N ), y );
+plot_multiple_lines( np.linspace( 0, N * dt, N ), y );
