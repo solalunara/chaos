6e0ad25853eb1a3f99b5f8efc373cefdd83a554e
solregem <greenberg04@gmail.com>
2024-03-12 12:10:42 +0000

    flex layout

--- webgl.js
+++ webgl.js
@@ -80,64 +80,66 @@ function loadShader(gl, type, source) {
     return shader;
 }
 
-const phaseDiv = document.querySelector( "#phasespace" );
-const realDiv = document.querySelector( "#realspace" );
-
-
-const phaseCanvas = document.querySelector( "#phaseCanvas" );
-const realCanvas = document.querySelector( "#realCanvas" );
-const gl_phase = phaseCanvas.getContext( "webgl" );
-const gl_real = realCanvas.getContext( "webgl" );
 
 window.addEventListener('load', function() {
+    const phaseDiv = document.querySelector( "#phasespace" );
+    const realDiv = document.querySelector( "#realspace" );
+
+
+    const phaseCanvas = document.querySelector( "#phaseCanvas" );
+    const realCanvas = document.querySelector( "#realCanvas" );
+    const gl_phase = phaseCanvas.getContext( "webgl" );
+    const gl_real = realCanvas.getContext( "webgl" );
+
     phaseCanvas.width = phaseDiv.getBoundingClientRect().width;
     phaseCanvas.height = phaseDiv.getBoundingClientRect().height;
     realCanvas.width = realDiv.getBoundingClientRect().width;
     realCanvas.height = realDiv.getBoundingClientRect().height;
+
+    if ( gl_phase === null || gl_real === null ) {
+        alert( "Unable to initialize WebGL. Your browser or machine may not support it." );
+    }
+
+    // Set clear color to black, fully opaque
+    gl_phase.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl_real.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl_phase.clear(gl_phase.COLOR_BUFFER_BIT);
+    gl_real.clear(gl_real.COLOR_BUFFER_BIT);
+
+    const phaseShaderProgram = initShaderProgram(gl_phase, vsSource, fsSource);
+    const realShaderProgram = initShaderProgram(gl_real, vsSource, fsSource);
+
+    const phaseProgramInfo = {
+        program: phaseShaderProgram,
+        attribLocations: {
+            vertexPosition: gl_phase.getAttribLocation(phaseShaderProgram, "aVertexPosition"),
+            vertexColor: gl_phase.getAttribLocation(phaseShaderProgram, "aVertexColor"),
+        },
+        uniformLocations: {
+            projectionMatrix: gl_phase.getUniformLocation(phaseShaderProgram, "uProjectionMatrix"),
+            modelViewMatrix: gl_phase.getUniformLocation(phaseShaderProgram, "uModelViewMatrix"),
+        },
+    };
+    const realProgramInfo = {
+        program: realShaderProgram,
+        attribLocations: {
+            vertexPosition: gl_real.getAttribLocation(realShaderProgram, "aVertexPosition"),
+            vertexColor: gl_real.getAttribLocation(realShaderProgram, "aVertexColor"),
+        },
+        uniformLocations: {
+            projectionMatrix: gl_real.getUniformLocation(realShaderProgram, "uProjectionMatrix"),
+            modelViewMatrix: gl_real.getUniformLocation(realShaderProgram, "uModelViewMatrix"),
+        },
+    };
+
+    // Here's where we call the routine that builds all the
+    // objects we'll be drawing.
+    const buffers_phase = initBuffers(gl_phase);
+    const buffers_real = initBuffers(gl_real);
+
+    // Draw the scene
+    drawScene(gl_phase, phaseProgramInfo, buffers_phase);
+    drawScene(gl_real, realProgramInfo, buffers_real);
 });
 
-if ( gl_phase === null || gl_real === null ) {
-    alert( "Unable to initialize WebGL. Your browser or machine may not support it." );
-}
-
-// Set clear color to black, fully opaque
-gl_phase.clearColor(0.0, 0.0, 0.0, 1.0);
-gl_real.clearColor(0.0, 0.0, 0.0, 1.0);
-// Clear the color buffer with specified clear color
-gl_phase.clear(gl_phase.COLOR_BUFFER_BIT);
-gl_real.clear(gl_real.COLOR_BUFFER_BIT);
-
-const phaseShaderProgram = initShaderProgram(gl_phase, vsSource, fsSource);
-const realShaderProgram = initShaderProgram(gl_real, vsSource, fsSource);
-
-const phaseProgramInfo = {
-    program: phaseShaderProgram,
-    attribLocations: {
-        vertexPosition: gl_phase.getAttribLocation(phaseShaderProgram, "aVertexPosition"),
-        vertexColor: gl_phase.getAttribLocation(phaseShaderProgram, "aVertexColor"),
-    },
-    uniformLocations: {
-        projectionMatrix: gl_phase.getUniformLocation(phaseShaderProgram, "uProjectionMatrix"),
-        modelViewMatrix: gl_phase.getUniformLocation(phaseShaderProgram, "uModelViewMatrix"),
-    },
-};
-const realProgramInfo = {
-    program: realShaderProgram,
-    attribLocations: {
-        vertexPosition: gl_real.getAttribLocation(realShaderProgram, "aVertexPosition"),
-        vertexColor: gl_real.getAttribLocation(realShaderProgram, "aVertexColor"),
-    },
-    uniformLocations: {
-        projectionMatrix: gl_real.getUniformLocation(realShaderProgram, "uProjectionMatrix"),
-        modelViewMatrix: gl_real.getUniformLocation(realShaderProgram, "uModelViewMatrix"),
-    },
-};
-
-// Here's where we call the routine that builds all the
-// objects we'll be drawing.
-const buffers_phase = initBuffers(gl_phase);
-const buffers_real = initBuffers(gl_real);
-
-// Draw the scene
-drawScene(gl_phase, phaseProgramInfo, buffers_phase);
-drawScene(gl_real, realProgramInfo, buffers_real);
